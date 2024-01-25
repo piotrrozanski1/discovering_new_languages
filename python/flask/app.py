4c4ebc00b6f80ae3c98ac1248b4d1247fa810fb8
piotrrozanski1 <156171694+piotrrozanski1@users.noreply.github.com>
2024-01-25 14:25:33 +0100

    slice html templates to small pieces (#6)
    Co-authored-by: Piotr Różański <piotr.rozanski@pwc.com>

--- python/flask/app.py
+++ python/flask/app.py
@@ -5,4 +5,39 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("home.html")
+
+
+@app.route("/shop")
+def shop():
+    return render_template("shop.html")
+
+
+@app.route("/cart")
+def cart():
+    return render_template("cart.html")
+
+
+@app.route("/checkout")
+def checkout():
+    return render_template("checkout.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/shop-detail")
+def shop_detail():
+    return render_template("shop-detail.html")
+
+
+@app.route("/testimonial")
+def testimonial():
+    return render_template("testimonial.html")
+
+
+@app.route("/no-found")
+def no_found():
+    return render_template("404.html")
