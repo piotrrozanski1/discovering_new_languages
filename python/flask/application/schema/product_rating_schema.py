28a12c4d0d068f9c38856418e802f9a6f9307f85
Piotr Różański <piotr.rozanski@pwc.com>
2024-02-01 17:03:38 +0100

    Add products entities

--- python/flask/application/schema/product_rating_schema.py
+++ python/flask/application/schema/product_rating_schema.py
@@ -1,8 +1,10 @@
 from configuration.database import ma
 
+
 class ProductRatingSchema(ma.Schema):
     class Meta:
         fields = ('id', 'rating', 'description')
+
 
 product_rating_schema = ProductRatingSchema(strict=True)
 products_ratings_schema = ProductRatingSchema(many=True, strict=True)
